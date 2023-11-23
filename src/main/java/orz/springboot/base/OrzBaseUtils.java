36c01a954605fa89cda0348aca6b01070e9cc2e7
reset7523 <reset7523@gmail.com>
2023-11-23 16:27:12 +0800

    Dev: improve base codes

--- src/main/java/orz/springboot/base/OrzBaseUtils.java
+++ src/main/java/orz/springboot/base/OrzBaseUtils.java
@@ -67,7 +67,7 @@ public class OrzBaseUtils {
         return Optional.ofNullable(result);
     }
 
-    public static <K, V> Map<K, V> newHashMapWithPairs(Object... pairs) {
+    public static <K, V> HashMap<K, V> newHashMapWithPairs(Object... pairs) {
         if (pairs == null || pairs.length == 0) {
             return new HashMap<>();
         }
@@ -84,46 +84,46 @@ public class OrzBaseUtils {
             map.put(k, v);
         }
         // noinspection unchecked
-        return (Map<K, V>) map;
+        return (HashMap<K, V>) map;
     }
 
-    public static <K, V> Map<K, V> newHashMap(K k1, V v1) {
+    public static <K, V> HashMap<K, V> newHashMap(K k1, V v1) {
         return newHashMapWithPairs(k1, v1);
     }
 
-    public static <K, V> Map<K, V> newHashMap(K k1, V v1, K k2, V v2) {
+    public static <K, V> HashMap<K, V> newHashMap(K k1, V v1, K k2, V v2) {
         return newHashMapWithPairs(k1, v1, k2, v2);
     }
 
-    public static <K, V> Map<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3) {
+    public static <K, V> HashMap<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3) {
         return newHashMapWithPairs(k1, v1, k2, v2, k3, v3);
     }
 
-    public static <K, V> Map<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4) {
+    public static <K, V> HashMap<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4) {
         return newHashMapWithPairs(k1, v1, k2, v2, k3, v3, k4, v4);
     }
 
-    public static <K, V> Map<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5) {
+    public static <K, V> HashMap<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5) {
         return newHashMapWithPairs(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5);
     }
 
-    public static <K, V> Map<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6) {
+    public static <K, V> HashMap<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6) {
         return newHashMapWithPairs(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6);
     }
 
-    public static <K, V> Map<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7) {
+    public static <K, V> HashMap<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7) {
         return newHashMapWithPairs(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7);
     }
 
-    public static <K, V> Map<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8) {
+    public static <K, V> HashMap<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8) {
         return newHashMapWithPairs(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7, k8, v8);
     }
 
-    public static <K, V> Map<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9) {
+    public static <K, V> HashMap<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9) {
         return newHashMapWithPairs(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7, k8, v8, k9, v9);
     }
 
-    public static <K, V> Map<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10) {
+    public static <K, V> HashMap<K, V> newHashMap(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10) {
         return newHashMapWithPairs(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5, k6, v6, k7, v7, k8, v8, k9, v9, k10, v10);
     }
 
